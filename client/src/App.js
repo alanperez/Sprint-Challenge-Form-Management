e2c6ea8f19eebf6ca9c98c7a2e2721ded0cd540b
Alan Perez <alanp9six@gmail.com>
2019-07-19 13:14:07 -0500

    :zap: Imported componenets and also wrapped the App with BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
+import RegisterForm from './components/RegisterForm'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import PrivateRoute from './components/config/PrivateRoute'
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App" style={{ padding: 30 }}>
+      <div>
+        <Link to="/">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
+      <Route exact path="/" component={Login} />
+      <Route path='/register'  component={RegisterForm}/>
+      <PrivateRoute exact path='/profile' component={Profile}/>
     </div>
   );
 }
